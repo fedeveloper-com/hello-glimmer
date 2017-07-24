71709a7e9edf3e77e5c5aefce16f42ceaa03fb9b
Nevcan Uludaş <nevcan.uludas@d-teknoloji.com.tr>
2017-07-24 16:04:55 +0300

    Created first sipmle component w GlimmerJS

--- src/ui/components/hello-glimmer/component.ts
+++ src/ui/components/hello-glimmer/component.ts
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
 
 export default class HelloGlimmer extends Component {
-
+    title = 'Hello Glimmer';
 }
---
+++ src/ui/components/logs-list/component.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class LogsList extends Component {
+    title = 'Logs List';
+};
